5be1cae46b49ddf3bed6726e58fedf4c08b12c23
ethan264624 <167527318+ethan264624@users.noreply.github.com>
2024-04-22 20:29:54 +1000

    Gradient
    Fixed an issue where the gradient would not show the correct destination point. This was fixed by opening up the "destination" dictionary.

--- Maths cat, real one.py
+++ Maths cat, real one.py
@@ -390,9 +390,9 @@ def main_function() : #main function, or the main game
                 x2, y2 = destination['x'], player1_information['y'] #same here
                 gradient = calculate_player1_gradient(x1, y1, x2, y2)
                 if gradient is not None: #if the gradient "is not None" is basically if the gradient isnt a straight line
-                    print("The gradient between player 1 ({}, {}) and the destination ({}, {}) is: {}".format(x1, y1, x2, y2, gradient))
+                    print("The gradient between player 1 ({}, {}) and the destination ({}, {}) is: {}".format(x1, y1, x2, destination['y'], gradient))
                 else: #.format replaces the empty brackets with the integers and the gradient value
-                    print("The coordinates ({}, {}) and ({}, {}) form a vertical line, so there is an undefined gradient.".format(x1, y1, x2, y2))
+                    print("The coordinates ({}, {}) and ({}, {}) form a vertical line, so there is an undefined gradient.".format(x1, y1, x2, destination['y']))
                     #shows when the gradient formed is a straight line
                 
                 #same as the above, just for player 2
@@ -405,9 +405,9 @@ def main_function() : #main function, or the main game
                 x2, y2 = destination['x'], player2_information['y']
                 gradient = calculate_player2_gradient(x1, y1, x2, y2)
                 if gradient is not None: 
-                    print("The gradient between player 2 ({}, {}) and the destination ({}, {}) is: {}".format(x1, y1, x2, y2, gradient))
+                    print("The gradient between player 2 ({}, {}) and the destination ({}, {}) is: {}".format(x1, y1, x2, destination['y'], gradient))
                 else: 
-                    print("The coordinates ({}, {}) and ({}, {}) form a vertical line, so there is an undefined gradient.".format(x1, y1, x2, y2))
+                    print("The coordinates ({}, {}) and ({}, {}) form a vertical line, so there is an undefined gradient.".format(x1, y1, x2, destination['y']))
                     
                 
              
